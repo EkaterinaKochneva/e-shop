d5354f96abd751072ed12f2e2bedb2d316b24ee8
EkaterinaKochneva <kochnevaes@gmail.com>
2022-05-25 18:09:03 +0300

    finish card

--- dist/js/app.js
+++ dist/js/app.js
@@ -26,7 +26,6 @@ window.onload = function () {
 	});
 
 	// Слайдеры
-
 	const swiper = new Swiper('.top-slider__swiper', {
 		loop: true,
 		autoplay: {
@@ -40,44 +39,46 @@ window.onload = function () {
 		slidesPerView: 1,
 	  });
 
-	  const swiperNew = new Swiper('.products-slider__swiper', {
-		slidesPerView: 1,		
-		loop: true,		
-		breakpoints: {
-			1401: {
-				slidesPerView: 5,
-				spaceBetween: 30,
-				autoHeight: true,
+	document.querySelectorAll('.products-slider').forEach(n => {
+		const swiperNew = new Swiper(n.querySelector('.products-slider__swiper'), {
+			slidesPerView: 1,		
+			loop: true,		
+			breakpoints: {
+				1401: {
+					slidesPerView: 5,
+					spaceBetween: 30,
+					autoHeight: true,
+				},
+				1199: {
+					slidesPerView: 4,
+					spaceBetween: 20,
+					autoHeight: true,
+				},
+				991: {
+					slidesPerView: 3,
+					autoHeight: true,
+				},
+				767: {
+					slidesPerView: 2,
+				}
 			},
-			1199: {
-				slidesPerView: 4,
-				spaceBetween: 20,
-				autoHeight: true,
-			},
-			991: {
-				slidesPerView: 3,
-				autoHeight: true,
-			},
-			767: {
-				slidesPerView: 2,
-			}
-		},
-		
-		navigation: {
+			
+			navigation: {
+				hide: false,
+				nextEl: n.querySelector('.swiper-button-next'),
+				prevEl: n.querySelector('.swiper-button-prev'),
+			  },
+			scrollbar: {
 			hide: false,
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		  },
-		scrollbar: {
-		hide: false,
-		el: '.products-slider__scrollbar',
-		draggable: true,
+			el: n.querySelector('.products-slider__scrollbar'),
+			draggable: true,
+	
+			},
+	
+		  });
+	});
 
-		},
-
-	  });
-
-	  const swiperExpertHelp = new Swiper('.expert-help__swiper', {
+	const swiperExpertHelp = new Swiper('.expert-help__swiper', {
 
 		slidesPerView: 1,
 
@@ -86,16 +87,16 @@ window.onload = function () {
 		  clickable: true,
 		},
 
-	  });
+	});
 
-	  const swiperProductThumbs = new Swiper('.product-thumbs__swiper', {
+	const swiperProductThumbs = new Swiper('.product-thumbs__swiper', {
 
 		slidesPerView: 'auto',
 		spaceBetween: 15,
 
-	  });
+	});
 
-	  const swiperProduct = new Swiper('.product-slider__swiper', {
+	const swiperProduct = new Swiper('.product-slider__swiper', {
 
 		slidesPerView: 1,
 		draggable: true,
@@ -103,33 +104,45 @@ window.onload = function () {
 		thumbs: {
 			swiper: swiperProductThumbs,
 		  },
-	  });
+	});
 
 
 // Аккардион FAQ
-
-		$('.faq__btn').click(function(){
-			if(!$(this).hasClass('active')){	
-				$(this).parents('.faq__wrapper').find('.faq__btn').removeClass('active'); 
-				$(this).parents('.faq__wrapper').find('.faq__answer').slideUp(); 
-				$(this).addClass('active');	
-				$(this).parent().next().slideDown();	
-			} else {	
-				$(this).removeClass('active');
-				$(this).parent().next().slideUp();
-			}
-		});
-
+	$('.faq__btn').click(function(){
+		if(!$(this).hasClass('active')){	
+			$(this).parents('.faq__wrapper').find('.faq__btn').removeClass('active'); 
+			$(this).parents('.faq__wrapper').find('.faq__answer').slideUp(); 
+			$(this).addClass('active');	
+			$(this).parent().next().slideDown();	
+		} else {	
+			$(this).removeClass('active');
+			$(this).parent().next().slideUp();
+		}
+	});
 
 // Tabs
+	$('.tabs__caption').on('click', '.tabs__btn:not(.active)', function (e) {
+		$(this)
+			.addClass('active').siblings().removeClass('active')
+			.closest('.tabs').find('.tabs__content').hide().removeClass('active')
+			.eq($(this).index()).fadeIn().addClass('active');
 
-		$('.tabs__caption').on('click', '.tabs__btn:not(.active)', function (e) {
-			$(this)
-				.addClass('active').siblings().removeClass('active')
-				.closest('.tabs').find('.tabs__content').hide().removeClass('active')
-				.eq($(this).index()).fadeIn().addClass('active');
+	});
 
-		});
+// Плавная прокрутка
+	$(".smooth-scroll-link-js").on("click", function (e) {
+		e.preventDefault();
+		var get_id = $(this).attr('href');
+		var target = $(get_id).offset().top; 
+		$("html, body").animate({ scrollTop: target }, 500);
+	});
+
+//Активация таба
+	$(".tabs-link-js").on("click", function (e) {
+		e.preventDefault();
+		var get_id = $(this).attr('href');
+		$(get_id)[0].click(); 
+	});
 
 // Фильтр категории
 
@@ -146,7 +159,7 @@ window.onload = function () {
 		parent.children(".category-filter__body").slideToggle();
 	});
 
-
+// Кастомный ползунок
 	function currencyFormat(num) {
 		return num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
 	} // currencyFormat(num)
@@ -214,19 +227,86 @@ window.onload = function () {
 		});
 	});
 
-}
+// Модалки
+	const link = ".link-modal-js";
+	$(link).fancybox({
+		arrows: false,
+		infobar: false,
+		touch: false,
+		type: 'inline',
+		autoFocus: false,
+		i18n: {
+			en: {
+				CLOSE: "Закрыть",
+				NEXT: "Вперед",
+				PREV: "Назад" // PLAY_START: "Start slideshow",
+				// PLAY_STOP: "Pause slideshow",
+				// FULL_SCREEN: "Full screen",
+				// THUMBS: "Thumbnails",
+				// DOWNLOAD: "Download",
+				// SHARE: "Share",
+				// ZOOM: "Zoom"
+
+			}
+		} // beforeLoad: function () {
+		// 	root.style.setProperty('--spacing-end', scrollWidth + 'px');
+		// },
+		// afterClose: function () {
+		// 	root.style.setProperty('--spacing-end', null);
+		// },
+
+	});
+	$(".modal-close-js").click(function () {
+		$.fancybox.close();
+	});
+	$.fancybox.defaults.backFocus = false;
+	const linkModal = document.querySelectorAll(link);
+
+	function addData() {
+		linkModal.forEach(element => {
+			element.addEventListener('click', () => {
+				let modal = document.querySelector(element.getAttribute("href"));
+				const data = element.dataset;
+
+				function setValue(val, elem) {
+					if (elem && val) {
+						const el = modal.querySelector(elem);
+						el.tagName == "INPUT" ? el.value = val : el.innerHTML = val; // console.log(modal.querySelector(elem).tagName)
+					}
+				}
+
+				setValue(data.title, '.ttu');
+				setValue(data.text, '.after-headline');
+				setValue(data.btn, '.btn');
+				setValue(data.order, '.order');
+			});
+		});
+	}
+
+	if (linkModal) addData();
+
+
+// mask for input
+let InputTel = [].slice.call(document.querySelectorAll('input[type="tel"]'));
+InputTel.forEach(element => element.setAttribute("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}"));
+Inputmask("+7(999)999-99-99", {
+	showMaskOnHover: false
+}).mask(InputTel);
+
 
 // Прокрутка наверх страницы
-$(window).on('scroll', function () {
-    if ($(this).scrollTop() > 100) {
-    $('.button-up').addClass('scroll');
-    } else {
-    $('.button-up').removeClass('scroll');
-    }
-    });
-    $('.button-up').click(function(){
-    $('body,html').animate({
-    scrollTop: 0
-    }, 500);
-    return false;
-});
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 100) {
+		$('.button-up').addClass('scroll');
+		} else {
+		$('.button-up').removeClass('scroll');
+		}
+		});
+		$('.button-up').click(function(){
+		$('body,html').animate({
+		scrollTop: 0
+		}, 500);
+		return false;
+	});
+
+}
